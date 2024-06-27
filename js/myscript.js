bf899be7d5564c75e63f231a38f1c7817a9632d3
Samgrass <samuelegrassetti01@gmail.com>
2024-06-27 16:46:09 +0200

    richiamo API, per creare una email random, in un ciclo da 10 e lo aggiungo in HTML FINE ESERCIZIO

--- js/myscript.js
+++ js/myscript.js
@@ -7,5 +7,16 @@ const { createApp } = Vue
 
             ],
       }
-    }
+    },
+    mounted() {
+        // utilizzo axios per richiamare l'api per creare una mail 10 volte
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(randomEmail => {
+                let email = randomEmail.data.response;
+                this.emails.push(email); 
+            }) 
+        } 
+        console.log(this.emails);
+    },
   }).mount('#app')
